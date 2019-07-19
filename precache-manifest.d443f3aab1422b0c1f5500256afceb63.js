9ef384d00fa21876c513dde1abe8cd1fdbdd2462
Yago Pereira <yagopazvd@gmail.com>
2019-07-19 14:54:22 -0300

    Updates

---
+++ precache-manifest.d443f3aab1422b0c1f5500256afceb63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f98859eb26a69895e06cc11986c636",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "084af4b643cc82e47f23",
+    "url": "/monsters-rolodex/static/css/main.fe1396b5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "084af4b643cc82e47f23",
+    "url": "/monsters-rolodex/static/js/main.d9a11866.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
